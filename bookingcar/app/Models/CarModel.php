c9eb9bb99e9eb6aeffa5dbc9935651954e3b4615
Wirunsak2001 <wc.psycho30@gmail.com>
2023-01-08 18:03:08 +0700

    tar

---
+++ bookingcar/app/Models/CarModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarModel extends Model
+{
+    use HasFactory;
+    protected $table = "tb_cars";
+    protected $fileable = [
+        'car_license',
+        'car_model',
+        'car_status'
+    ];
+}
