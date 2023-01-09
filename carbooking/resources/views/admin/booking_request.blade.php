d196428f5aee6985f2377aca534ad992f1f74ffc
Jirasak07 <bee.110243@gmail.com>
2023-01-09 22:19:54 +0700

    up

--- carbooking/resources/views/admin/booking_request.blade.php
+++ carbooking/resources/views/admin/booking_request.blade.php
@@ -65,73 +65,106 @@
                             </label>
                         </div>
                     </div>
-                    {{-- <div id="myDiv">
-                    </div> --}}
-                    <form action="{{ route('GG') }}" method="post">
-                        @csrf
-                        <div class="form-control py-5 h-100 d-flex flex-row justify-content-around">
-                            <div class=" me-2 col-6">
-                                <label class="form-label">เลือกรถที่ใช้</label>
-                                <select name="car" class="form-select rounded px-3 py-2 w-100 "
-                                    aria-label="Default select example">
-                                    <option selected value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                            </div>
-                            <div class=" col-6">
-                                <label class="form-label">เลือกพนักงานขับรถ</label>
-                                <select name="driver" class="form-select rounded px-3 py-2 w-100 "
-                                    aria-label="Default select example">
-                                    <option selected value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                            </div>
-
-                        </div>
-
-
-                        <div class="modal-footer">
-                            <input type="submit" name="submit" class="btn btn-success" value="อนุมัติ" />
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ปิด</button>
-                        </div>
-                    </form>
+                    <div id="myDiv"></div>
                 </div>
 
             </div>
         </div>
     </div>
     <script>
-        // document.addEventListener('DOMContentLoaded', function() {
-        //     document.getElementById('myDiv').innerHTML = '<form class="bg-danger" action="/form1" method="post">' +
-        //         '@csrf' +
-        //         '<input type="text" name="field1" placeholder="Field 1">' +
-        //         '<button type="submit">Submit</button>' +
-        //         '</form>';
-        // })
+        document.addEventListener('DOMContentLoaded', function() {
+            document.getElementById('myDiv').innerHTML =
+                ' <form action="{{ route('GG') }}" method="post">' +
+                '@csrf' +
+                '<div class="form-control py-5 h-100 d-flex flex-row justify-content-around">' +
+                ' <div class=" me-2 col-6">' +
+                ' <label class="form-label">เลือกรถที่ใช้</label>' +
+                '<select name="car" class="form-select rounded px-3 py-2 w-100 " aria-label="Default select example">' +
+                '  <option selected value="1">One</option>' +
+                ' <option value="2">Two</option>' +
+                '  <option value="3">Three</option>' +
+                '    </select>' +
+                '   </div>' +
+                '  <div class=" col-6">' +
+                ' <label class="form-label">เลือกพนักงานขับรถ</label>' +
+                '  <select name="driver" class="form-select rounded px-3 py-2 w-100 "aria-label="Default select example">' +
+                '<option selected value="1">One</option>' +
+                '  <option value="2">Two</option>' +
+                '  <option value="3">Three</option>' +
+                '     </select>' +
+                ' </div>' +
+                '    </div>' +
+                '   <div class="modal-footer">' +
+                ' <input type="submit" name="submit" class="btn btn-success" value="อนุมัติ" />' +
+                '  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ปิด</button>' +
+                '    </div>' +
+                '  </form>';
+        })
 
-        // function chkform() {
-        //     var rd1 = document.getElementById('car1');
-        //     var rd2 = document.getElementById('car2');
-        //     if (rd1.checked == true) {
-        //         var form = "1";
-        //         document.getElementById('myDiv').innerHTML =
-        //             '<form  action="{{ route('GG') }}" method="post">' +
-        //             '@csrf' +
-        //             '<input type="text" name="field1" placeholder="Field 1">' +
-        //             ' <div class="modal-footer">' +
-        //             ' <input type="submit" name="submit" class="btn btn-success" value="อนุมัติ" />' +
-        //             ' <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ปิด</button>' +
-        //             '</div>' +
-        //             '</form>';
-        //     } else if (rd2.checked == true) {
-        //         var form = "2";
-        //         document.getElementById('myDiv').innerHTML = '<form action="{{ route('GG') }}" method="post">' +
-        //             '@csrf' +
-        //             '<input type="text" name="field2" placeholder="Field 2">' +
-        //             '</form>';
-        //     }
-        // }
+        function chkform() {
+            var rd1 = document.getElementById('car1');
+            var rd2 = document.getElementById('car2');
+            if (rd1.checked == true) {
+                var form = "1";
+                document.getElementById('myDiv').innerHTML =
+                    ' <form action="{{ route('GG') }}" method="post">' +
+                    '@csrf' +
+                    '<div class="form-control py-5 h-100 d-flex flex-row justify-content-around">' +
+                    ' <div class=" me-2 col-6">' +
+                    ' <label class="form-label">เลือกรถที่ใช้</label>' +
+                    '<select name="car" class="form-select rounded px-3 py-2 w-100 " aria-label="Default select example">' +
+                    '  <option selected value="1">One</option>' +
+                    ' <option value="2">Two</option>' +
+                    '  <option value="3">Three</option>' +
+                    '    </select>' +
+                    '   </div>' +
+                    '  <div class=" col-6">' +
+                    ' <label class="form-label">เลือกพนักงานขับรถ</label>' +
+                    '  <select name="driver" class="form-select rounded px-3 py-2 w-100 "aria-label="Default select example">' +
+                    '<option selected value="1">One</option>' +
+                    '  <option value="2">Two</option>' +
+                    '  <option value="3">Three</option>' +
+                    '     </select>' +
+                    ' </div>' +
+                    '    </div>' +
+                    '   <div class="modal-footer">' +
+                    ' <input type="submit" name="submit" class="btn btn-success" value="อนุมัติ" />' +
+                    '  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ปิด</button>' +
+                    '    </div>' +
+                    '  </form>';
+            } else if (rd2.checked == true) {
+                var form = "2";
+                document.getElementById('myDiv').innerHTML =
+                    '<form action="{{ route('GG') }}" method="post">' +
+                    '@csrf' +
+                    '  <div class=" mt-4 d-flex flex-row row justify-content-center">' +
+                    ' <div class="mb-3 col-5">' +
+                    ' <label for="exampleInputEmail1" class="form-label">ป้ายทะเบียน</label>' +
+                    '<input type="text" class="form-control" name="license" />' +
+                    '  </div>' +
+                    ' <div class="mb-3 col-5">' +
+                    '    <label for="exampleInputEmail1" class="form-label">ยี่ห้อ</label>' +
+                    '   <input type="text" class="form-control" name="brand" />' +
+                    ' </div>' +
+                    '<div class="mb-3 col-10">' +
+                    ' <label for="exampleInputEmail1" class="form-label">รุ่น</label>' +
+                    ' <input type="text" class="form-control" name="model" />' +
+                    '   </div>' +
+                    ' <div class="mb-3 col-5">' +
+                    '<label for="exampleInputEmail1" class="form-label">เจ้าของรถ</label>' +
+                    '<input type="text" class="form-control" name="out_name" />' +
+                    ' </div>' +
+                    ' <div class="mb-3 col-5">' +
+                    '   <label for="exampleInputEmail1" class="form-label">เบอร์โทรศัพท์</label>' +
+                    ' <input type="text" class="form-control" name="out_name" />' +
+                    ' </div>' +
+                    '     </div>' +
+                    ' <div class="modal-footer">' +
+                    ' <input type="submit" name="submit" class="btn btn-success" value="อนุมัติ" />' +
+                    ' <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ปิด</button>' +
+                    ' </div>' +
+                    '</form>';
+            }
+        }
     </script>
 @endsection
