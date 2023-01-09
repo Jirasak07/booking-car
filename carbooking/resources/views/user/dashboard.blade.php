9255ad42c67635368e0d952e8d144af353ff0ba8
my git btoiandw <bbdbiw2544@gmail.com>
2023-01-09 22:36:18 +0700

    edit calendar

--- carbooking/resources/views/user/dashboard.blade.php
+++ carbooking/resources/views/user/dashboard.blade.php
@@ -6,16 +6,7 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="card">
-                    <div class="card-header bg-transparent">
-                        <div class="row align-items-center">
-                            <div class="col">
-                                <h6 class="text-uppercase text-muted ls-1 mb-1">booking</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        @include('user.calendar')
-                    </div>
+                    @include('user.calendar')
                 </div>
             </div>
         </div>
