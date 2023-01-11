797de76d3ddabc745f2fed928f3bcfe51dd50ea5
Jirasak07 <bee.110243@gmail.com>
2023-01-11 09:51:17 +0700

    up

--- carbooking/resources/views/admin/dashboard.blade.php
+++ carbooking/resources/views/admin/dashboard.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.admin.admin')
 @section('content')
     @include('layouts.admin.header')
-    <div class="container-fulid mx-5  ">
-        <div class="d-flex flex-column justify-content-center align-items-center">
-            <div  class=" col-12  flex-column d-flex justify-content-between align-items-center ">
+    <div class="container ">
+        <div class="d-flex flex-column justify-content-center align-items-center ">
+            <div class=" col-12  flex-column d-flex justify-content-between align-items-center  ">
                 {{-- ///////////////////// --}}
-                <div class="box h-100 col-md col align-self-start p-3">
+                <div class="box h-100 col-md-12 col-12 align-self-start p-3">
                     <div class="row p-2" style="gap: 10px">
                         @foreach ($car as $cars)
-                            <div class=" col-12 col-sm-5 col-lg col-md-12 shadow-box rounded ">
-                                <div class=" box-1 bg-dark-2 box p-3">
+                            <div class=" col-12 col-sm-12 col-lg col-md-12 shadow-box rounded ">
+                                <div class=" box-1  box p-3">
                                     <div class="d-flex row ">
                                         <div class="col ">
                                             <div class="d-flex flex-row align-items-center">
@@ -30,79 +30,77 @@
                                         </div>
                                     </div>
                                 </div>
-
-
                             </div>
                         @endforeach
                     </div>
                 </div>
-                {{-- ////////////////////////////// --}}
-                <div class=" col-md-4 col-12 row w-100 " style="height: 100%; max-height:350px">
-                    <div class=" col-12 bg-dark-2 h-100 w-100 rounded ">
-                        <div class="  rounded h-100 w-100 " style=" height:100%; width:100%; ">
+            </div>
+            <div class="col-12 d-flex flex-column flex-lg-row justify-content-between align-items-center p-1">
+                <div class="rounded shadow-box box-1 box col-md-12 col-sm-12 col-lg-4 col-12 row w-100 "
+                    style="height: 100%; max-height:350px">
+                    <div class=" col-12  h-100 w-100 rounded ">
+                        <div class="  rounded h-100 w-100 " style=" height:100%; width:100%;  ">
                             <canvas id="myChart"></canvas>
                         </div>
-
                     </div>
-
+                </div>
+                <div class="rounded shadow-box box-1 box col-md-12 col-sm-12 col-lg-4 col-12 row w-100 "
+                style="height: 100%; max-height:350px">
+                <div class=" col-12   rounded ">
+                    <div class="  rounded " style=" min-height:300px; width:100%;  ">
+                        <canvas class="h-100" id="myChart">1</canvas>
+                    </div>
                 </div>
             </div>
-            <div class=" col-12 flex-md-row flex-column d-flex justify-content-between align-items-center  mt-2 p-3 ">
-                <div class="p-2 col-12 col-md-8">
-                    <div class="bg-dark-2 rounded">
-                         <div id="Chart">cvcvc</div>
-                    </div>
-
-                </div>
-                <div class=" col p-2 ">
-                    <div class="bg-dark-2 rounded" >dgdg</div>
+                <div class="rounded shadow-box box-1 box col-md-12 col-sm-12 col-lg-4 col-12 row w-100">
+                    <div class="h-100">2</div>
                 </div>
             </div>
-            {{-- ///////////////////////////////////////////////////////////////////////////////////////// --}}
-
-
         </div>
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                const ctx = document.getElementById('myChart');
-                const bar = document.getElementById('Chart');
-                new Chart(ctx, {
-                    type: 'doughnut',
+        <div class="d-flex row flex-lg-row flex-column justify-content-center align-items-center p-3 ">
+        </div>
+    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const ctx = document.getElementById('myChart');
+            const bar = document.getElementById('Chart');
+            new Chart(ctx, {
+                type: 'doughnut',
 
-                    data: data = {
-                        labels: [
-                            'รถภายใน',
-                            'รถภายนอก',
+                data: data = {
+                    labels: [
+                        'รถภายใน',
+                        'รถภายนอก',
+
+                    ],
+                    datasets: [{
+                        label: 'มีการใช้งาน ครั้ง',
+                        data: [5, 2],
+                        backgroundColor: [
+                            '#205295',
+                            '#EFEFEF',
 
                         ],
-                        datasets: [{
-                            label: 'มีการใช้งาน ครั้ง',
-                            data: [5, 2],
-                            backgroundColor: [
-                                '#205295',
-                                '#EFEFEF',
+                        hoverOffset: 4
+                    }]
+                },
+                options: {
 
-                            ],
-                            hoverOffset: 4
-                        }]
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
                     },
-                    options: {
-
-                        maintainAspectRatio: false,
-                        responsive: true,
-                        scales: {
-                            y: {
-                                beginAtZero: true
-                            }
-                        },
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: 'สัดส่วนการใช้งานของรถภายในและรถภายนอก'
-                            }
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'สัดส่วนการใช้งานของรถภายในและรถภายนอก'
                         }
                     }
-                });
-            })
-        </script>
-    @endsection
+                }
+            });
+        })
+    </script>
+@endsection
