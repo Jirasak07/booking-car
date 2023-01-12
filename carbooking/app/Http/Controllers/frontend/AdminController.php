4ef03100c4879ebc283b4619a801c31736e9fad6
my git btoiandw <bbdbiw2544@gmail.com>
2023-01-12 09:28:29 +0700

    q

--- carbooking/app/Http/Controllers/frontend/AdminController.php
+++ carbooking/app/Http/Controllers/frontend/AdminController.php
@@ -8,10 +8,10 @@ use App\Models\CarModel;
 class AdminController extends Controller
 {
     //
-    public function __construct()
+    /* public function __construct()
     {
         $this->middleware('auth');
-    }
+    } */
     public function index()
     {
         $car = CarModel::All();
