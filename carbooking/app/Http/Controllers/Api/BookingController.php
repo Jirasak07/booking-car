a5a763d9d937b387a80c4cfc1006aa7e1f477af6
Wirunsak2001 <wc.psycho30@gmail.com>
2023-01-13 10:04:41 +0700

    tt

--- carbooking/app/Http/Controllers/Api/BookingController.php
+++ carbooking/app/Http/Controllers/Api/BookingController.php
@@ -30,7 +30,7 @@ class BookingController extends Controller
         ->get();
         foreach($bookings as $item){
             $events[] = [
-                'title' => $item->booking_id,
+                'id' => $item->booking_id,
                 'start' => $item->booking_start,
                 'end' => $item->booking_end,
                 'type' => $item->type_car
