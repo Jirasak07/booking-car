5f338ddb53b6329e5f2973c699d45e5066a202d9
Wirunsak2001 <wc.psycho30@gmail.com>
2023-01-16 15:53:35 +0700

    dsa

---
+++ carbooking/app/Http/Controllers/backend/UserBookingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\BookingModel;
+use Illuminate\Http\Request;
+
+class UserBookingController extends Controller
+{
+    //
+    function edit_booking(Request $request,$id){
+        $booking = BookingModel::find($id);
+        $booking->booking_start = $request->booking_start;
+        $booking->booking_end = $request->booking_end;
+        $booking->booking_detail = $request->booking_detail;
+        $booking->seve();
+        return redirect()->back();
+    }
+}
