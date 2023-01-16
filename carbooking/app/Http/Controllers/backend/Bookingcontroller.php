7a2344119af0d0d3ce10bd4a4bac1ed2a4c4d6df
my git btoiandw <bbdbiw2544@gmail.com>
2023-01-16 08:58:41 +0700

    ss

--- carbooking/app/Http/Controllers/backend/Bookingcontroller.php
+++ carbooking/app/Http/Controllers/backend/Bookingcontroller.php
@@ -33,14 +33,14 @@ class Bookingcontroller extends Controller
 
         $booking = DB::table('tb_booking')
         
-        ->join('Users', 'tb_booking.username', '=', 'Users.id')
+        ->join('users', 'tb_booking.username', '=', 'users.id')
         
         ->join('tb_cars', 'tb_booking.license_plate', '=', 'tb_cars.id')
         ->join('tb_out_cars', 'tb_booking.license_plate', '=', 'tb_out_cars.id')
         ->join('tb_driver', 'tb_booking.driver', '=', 'tb_driver.id')
-        ->whereIn('Users.id', Auth::Users()->id)
+        ->whereIn('users.id', Auth::user())
         
-        ->select( 'car_out_license', 'car_out_model', 'car_out_driver', 'car_out_tel',  'driver_fullname', 'car_license','tb_booking.*' ,'Username')
+        ->select( 'car_out_license', 'car_out_model', 'car_out_driver', 'car_out_tel',  'driver_fullname', 'car_license','tb_booking.*' ,'users.username')
         ->get();
         // return dd($booking);
         
