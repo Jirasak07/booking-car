5cf1cc03d15d3bcc5f8a803d8818b4293d9474eb
Jirasak07 <bee.110243@gmail.com>
2023-01-20 12:44:20 +0700

    Update DashboardAdminController.php

--- carbooking/app/Http/Controllers/backend/DashboardAdminController.php
+++ carbooking/app/Http/Controllers/backend/DashboardAdminController.php
@@ -113,19 +113,18 @@ class DashboardAdminController extends Controller
             ->join('users', 'tb_booking.username', '=', 'users.id')
             ->join('tb_driver', 'tb_booking.driver', '=', 'tb_driver.id')
             ->where('tb_booking.id','=', $id)
-            ->select('tb.driver.driver_fullname', 'tb_cars.car_license','tb_booking.*','users.name')
+            ->select('tb_driver.driver_fullname', 'tb_cars.car_license','tb_booking.*','users.name')
             ->get();
         }elseif($booking->typecar ==2){
             $detail = DB::table('tb_booking')
             ->join('users', 'tb_booking.username', '=', 'users.id')
             ->join('tb_out_cars', 'tb_booking.license_plate', '=', 'tb_out_cars.id')
-            
+
             ->where('tb_booking.id','=', $id)
             ->select('car_out_license', 'car_out_model', 'car_out_driver', 'car_out_tel','tb_booking.*','users.name')
             ->get();
         }else{
             $detail = DB::table('tb_booking')
-            
             ->join('tb_out_cars', 'tb_booking.license_plate', '=', 'tb_out_cars.id')
             ->join('users', 'tb_booking.username', '=', 'users.id')
             ->where('tb_booking.id','=', $id)
@@ -133,11 +132,11 @@ class DashboardAdminController extends Controller
             ->get();
         }
 
-        
+
         return response()->json([
             'detail' =>$detail
         ]);
-       
+
     }
 
 }
