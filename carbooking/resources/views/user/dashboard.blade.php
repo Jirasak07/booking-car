e64512143bf6e2a984b673362ab7cdd82adce21a
my git btoiandw <bbdbiw2544@gmail.com>
2023-01-31 14:54:54 +0700

    calendar

--- carbooking/resources/views/user/dashboard.blade.php
+++ carbooking/resources/views/user/dashboard.blade.php
@@ -3,35 +3,48 @@
 @section('content')
     @include('layouts.header')
     <div class="container-fluid">
-        <div class="row">
-            @if ($message = Session::get('success'))
-                <script>
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'การจองสำเร็จ',
-                        text: 'โปรดรอแอดมินอนุมัติ',
-                    });
-                </script>
-            @endif
-            @if ($errors->any())
-                <div class="alert alert-danger" id="ERROR_COPY" style="display:none;">
-                    <ul style="list-style: none;">
-                        @foreach ($errors->all() as $error)
-                            <!-- ทำการ วน Loop เพื่อแสดง Error ของ validation ขึ้นมาทั้งหมด -->
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
 
-            {{-- <div class="col-xl-12">@include('user.calendar')
+        @if ($message = Session::get('success'))
+            <script>
+                Swal.fire({
+                    icon: 'success',
+                    title: 'การจองสำเร็จ',
+                    text: 'โปรดรอแอดมินอนุมัติ',
+                });
+            </script>
+        @endif
+        @if ($errors->any())
+            <div class="alert alert-danger" id="ERROR_COPY" style="display:none;">
+                <ul style="list-style: none;">
+                    @foreach ($errors->all() as $error)
+                        <!-- ทำการ วน Loop เพื่อแสดง Error ของ validation ขึ้นมาทั้งหมด -->
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        {{-- <div class="col-xl-12">@include('user.calendar')
                 <div class="card mt-5">
 
 
                 </div>
             </div> --}}
-        </div>
-        <div class="bg-white mb-3 mt-3 rounded " style="height:150vh">
+
+        <div class="card px-3 my-3 py-3 shadow-xl" style="height:150vh">
+            <div class="row pb-3 pl-3">
+                <strong class=" text-danger">หมายเหตุ : </strong>
+                &ensp;
+                <div class="" style="font-size: 14px;">
+                    <i class="fa-solid fa-square"style="color: #06d6a0"></i>
+                    อนุมัติเรียบร้อย
+                </div>
+                &emsp;
+                <div class="" style="font-size: 14px;">
+                    <i class="fa-solid fa-square"style="color: #ffd166"></i>
+                    รอดำเนินการ
+                </div>
+            </div>
             @include('user.calendar')
         </div>
     </div>
