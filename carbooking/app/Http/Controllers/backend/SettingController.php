31dbed346aba29d4135c3058dd71b1fa90dc8c40
Wirunsak2001 <wc.psycho30@gmail.com>
2023-02-01 14:59:19 +0700

    dsa

--- carbooking/app/Http/Controllers/backend/SettingController.php
+++ carbooking/app/Http/Controllers/backend/SettingController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\backend;
 use App\Http\Controllers\Controller;
 use App\Models\timebookingModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SettingController extends Controller
 {
     //
+    public function index(){
+        $setting = timebookingModel::all();
+        return view('')->with(['time'=>$setting]);
+    }
 
     public function edit_time(Request $request){
         $id = $request->id_form;
