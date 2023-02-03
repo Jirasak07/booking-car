5e80401083db5e40ccef569b93bb69c3e3e3debe
Jirasak07 <bee.110243@gmail.com>
2023-02-03 14:50:25 +0700

    Update booking_request.blade.php

--- carbooking/resources/views/admin/booking_request.blade.php
+++ carbooking/resources/views/admin/booking_request.blade.php
@@ -257,21 +257,14 @@
                                 type: "POST",
                                 data: frm,
                                 success: function(response) {
-                                    const Toast = Swal.mixin({
-                                        toast: true,
-                                        position: 'top-end',
-                                        showConfirmButton: false,
-                                        timer: 3000,
-                                        timerProgressBar: true,
-                                        didOpen: (toast) => {
-                                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                                            toast.addEventListener('mouseleave', Swal
-                                                .resumeTimer)
-                                        }
-                                    })
-                                    Toast.fire({
+
+                                    Swal.fire({
                                         icon: 'success',
                                         title: 'อนุมัติเสร็จสิ้น',
+                                        timer: 1500,
+                                        showConfirmButton:false,
+                                        timerProgressBar: true,
+
                                     }).then((res) => {
                                         window.location.reload()
                                     })
