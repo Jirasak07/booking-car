717e44ad6bbfb229698d7ba0aab0732f7968cd86
Wirunsak2001 <wc.psycho30@gmail.com>
2023-02-07 14:06:08 +0700

    d

--- carbooking/app/Mail/SendEmailComponent.php
+++ carbooking/app/Mail/SendEmailComponent.php
@@ -28,6 +28,6 @@ class SendEmailComponent extends Mailable
      */
     public function build()
     {
-        return $this->markdown('email.sendEmailComponent');
+        return $this->markdown('emails.sendEmailComponent');
     }
 }
