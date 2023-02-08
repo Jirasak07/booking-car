6329336a1d36bc90d910849169e54560a1b37868
Jirasak07 <bee.110243@gmail.com>
2023-02-08 17:28:38 +0700

    Update layout.blade.php

--- carbooking/resources/views/layouts/layout.blade.php
+++ carbooking/resources/views/layouts/layout.blade.php
@@ -61,7 +61,25 @@
             var channel = pusher.subscribe('store-channel');
             channel.bind('store-booking', function(data) {
                 console.log(data)
+                toastr.options = {
+                    closeButton: true,
+                    debug: false,
+                    newestOnTop: false,
+                    progressBar: false,
+                    positionClass: "toast-top-right",
+                    preventDuplicates: false,
+                    onclick: null,
+                    showDuration: "300",
+                    hideDuration: "1000",
+                    timeOut: "5000",
+                    extendedTimeOut: "1000",
+                    showEasing: "swing",
+                    hideEasing: "linear",
+                    showMethod: "fadeIn",
+                    hideMethod: "fadeOut"
+                };
                 toastr.warning(data['message'])
+
                 // const Toast = Swal.mixin({
                 //     customClass: {
                 //         confirmButton: 'btn btn-sm btn-warning',
