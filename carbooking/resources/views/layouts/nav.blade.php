1cb274e3f9f987312b3090757377d2c8df09518b
Jirasak07 <bee.110243@gmail.com>
2023-02-14 16:31:41 +0700

    Update nav.blade.php

--- carbooking/resources/views/layouts/nav.blade.php
+++ carbooking/resources/views/layouts/nav.blade.php
@@ -22,16 +22,22 @@
 
         <!-- User -->
         <ul class="navbar-nav  align-items-center d-none d-md-flex">
+
             <li class="nav-item dropdown">
                 <a class=" pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                     aria-expanded="false">
                     <div class="media align-items-center ">
+                        <i class="fa-solid fa-bell mr-2 text-white position-relative" style="font-size: 20px"> <span
+                                class="position-absolute  badge bottom-2 right-2 bg-danger d-flex justify-content-center " style=" border-radius:50%;width:25px;height:25px;font-size:5px " >
+                                3
+                            </span> </i>
+                        <h4 class="text-overflow m-0 text-light px-2">{{ Auth::user()->name }}</h4>
                         <span class="avatar avatar-sm rounded-circle " style="background-color:#F5A65B;">
                             {{-- <img alt="Image placeholder" src="{{ asset('assets/img/lanna-removebg-preview.png') }}"> --}}
+
                             <i class="fa-solid fa-user-tie" style="font-size: 1.5rem"></i>
 
                         </span>
-                        <h4 class="text-overflow m-0 text-light px-2">{{ Auth::user()->name }}</h4>
                         <div class="media-body ml-2 d-none d-lg-block">
                             <span class="mb-0 text-sm  font-weight-bold">{{-- {{ auth()->user()->name }} --}}</span>
                         </div>
