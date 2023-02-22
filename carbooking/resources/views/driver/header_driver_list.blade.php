74b43960b942919dbe20322038270f25c7e46ac6
btoiandw <bbdbiw2544@gmail.com>
2023-02-22 17:29:26 +0700

    driver blade edit

--- carbooking/resources/views/driver/header_driver_list.blade.php
+++ carbooking/resources/views/driver/header_driver_list.blade.php
@@ -2,28 +2,26 @@
 <div class="container-fluid mt-4 ">
     <div class="d-flex flex-xl-row flex-column" style="gap: 10px;min-height:120px">
         <div class="card w-100 rounded text-darker" style="background-color: #427AA1;">
-            <div class=" rounded h-100 d-flex flex-row align-items-center"
-                style="min-height:120px;">
+            <div class=" rounded h-100 d-flex flex-row align-items-center" style="min-height:120px;">
                 <div class="h-100 col icon-d-2 icon-circle text-white">
                     <i class="fa-solid fa-briefcase icon-dashboard"></i>
                 </div>
                 <div class="col d-flex flex-column align-items-center text-white">
                     <div class="">ทั้งหมด</div>
-                    <div style="font-size: 4rem;line-height: 80%;"id="alllist">0</div>
+                    <div style="font-size: 4rem;line-height: 80%;"id="alllist">{{ $sumbooking }}</div>
                     <div>รายการ </div>
                 </div>
             </div>
         </div>
 
         <div class=" card w-100 rounded" style="background-color: #FFB400aa;">
-            <div class=" rounded h-100 d-flex flex-row align-items-center"
-                style="min-height:120px;">
+            <div class=" rounded h-100 d-flex flex-row align-items-center" style="min-height:120px;">
                 <div class="h-100 col icon-d-2 icon-circle">
                     <i class="fa-solid fa-hourglass-end icon-dashboard "></i>
                 </div>
                 <div class="col d-flex flex-column align-items-center text-white">
                     <div>รอดำเนินการ</div>
-                    <div style="font-size: 4rem;line-height: 80%;" id="alllistpending">0</div>
+                    <div style="font-size: 4rem;line-height: 80%;" id="alllistpending">{{ $padding }}</div>
                     <div>รายการ</div>
                 </div>
             </div>
@@ -31,7 +29,7 @@
         <div class=" card w-100 rounded" style="background-color: #9a8c98;">
             <div class="rounded h-100 d-flex flex-row align-items-center"style="min-height:120px;">
                 <div class="h-100 col icon-d-2 icon-circle">
-                    <i class="fa-solid fa-road icon-dashboard" ></i>
+                    <i class="fa-solid fa-road icon-dashboard"></i>
                 </div>
                 <div class="col d-flex flex-column align-items-center text-white">
                     <div>ระยะทาง</div>
@@ -42,8 +40,7 @@
         </div>
 
         <div class=" card w-100 rounded" style="background-color: #8d99ae;">
-            <div class="rounded h-100 w-100 d-flex flex-row align-items-center"
-                style="min-height:120px;">
+            <div class="rounded h-100 w-100 d-flex flex-row align-items-center" style="min-height:120px;">
                 <div class=" h-100 icon-d-2 icon-circle col">
                     <i class="fa-solid fa-star icon-dashboard" style="color:#ffb703"></i>
                 </div>
@@ -57,3 +54,6 @@
     </div>
 
 </div>
+
+@push('js')
+@endpush
