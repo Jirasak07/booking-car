779c49a669d8724dc6b859daf5db5a6cf682eaf1
btoiandw <bbdbiw2544@gmail.com>
2023-02-22 16:55:34 +0700

    add 1 minutes for end booking

--- carbooking/resources/views/user/calendar.blade.php
+++ carbooking/resources/views/user/calendar.blade.php
@@ -124,7 +124,7 @@
                     var b_start = moment(info.startStr);
                     var b_end = moment(info.endStr);
 
-                    var booking_e = moment(b_end,'HH:mm:ss a').add(1,'minutes').format('YYYY-MM-DD HH:mm')
+                    var booking_e = moment(b_end,'HH:mm:ss a').add(1,'minutes').format('YYYY-MM-DD HH:mm');
                     //console.log('booking_e :'+booking_e);
                     $.ajax({
                         url: '/users/validate_booking',
@@ -176,7 +176,7 @@
                                 // $('#booking_start').html(booking_start);
                                 // $('#booking_end').html(booking_end);
                                 document.getElementById('date_start').value = booking_start;
-                                document.getElementById('date_end').value = booking_end;
+                                document.getElementById('date_end').value = booking_e;
 
                                 document.getElementById('location').focus();
                                 //tag input datetime-local เลือกวันย้อนหลังไม่ได้
