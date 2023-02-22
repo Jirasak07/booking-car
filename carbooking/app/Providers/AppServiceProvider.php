2edc838b8623a58f646bb2487dee955dae6009ed
Wirunsak2001 <wc.psycho30@gmail.com>
2023-02-22 13:11:58 +0700

    hhh

--- carbooking/app/Providers/AppServiceProvider.php
+++ carbooking/app/Providers/AppServiceProvider.php
@@ -25,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
             ->where('booking_start', '<', Carbon::now()->subMinutes(15))
             ->update(['booking_status'=>'3','booking_detail'=>'booking_detail'.'~'.'หมดเวลาการจองแล้ว']);
     }
+
+    public function autobooking(){
+        $current_date_time = Carbon::now();
+        DB::table('tb_booking')
+            ->where('booking_status', '2')
+            ->where('booking_start', '<', Carbon::now())
+            ->update(['booking_status'=>'4']);
+    }
     /**
      * Bootstrap any application services.
      *
