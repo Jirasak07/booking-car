11e2475109368ba04265423cf299d63124bdd564
btoiandw <bbdbiw2544@gmail.com>
2023-02-23 09:19:18 +0700

    dsadsa

--- carbooking/public/firebase-messaging-sw.js
+++ carbooking/public/firebase-messaging-sw.js
@@ -4,7 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-   'messagingSenderId': '567315129019'
+   'messagingSenderId': '459977334080'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
